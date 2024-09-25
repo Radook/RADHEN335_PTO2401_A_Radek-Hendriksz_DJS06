d3c9b8d355287b45983f39dfb4f41f9505c6e0a9
Radek <radekhendriksz@gmail.com>
2024-09-25 06:10:55 +0200

    first commit

---
+++ scripts.js
@@ -0,0 +1,28 @@
+// A list of provinces:
+const provinces = ['Western Cape', 'Gauteng', 'Northern Cape', 'Eastern Cape', 'KwaZulu-Natal', 'Free State']
+
+// A list of names:
+const names = ['Ashwin', 'Sibongile', 'Jan-Hendrik', 'Sifso', 'Shailen', 'Frikkie']
+
+// A list of products with prices:
+const products = [
+  { product: 'banana', price: "2" },
+  { product: 'mango', price: 6 },
+  { product: 'potato', price: ' ' },
+  { product: 'avocado', price: "8" },
+  { product: 'coffee', price: 10 },
+  { product: 'tea', price: '' },
+]
+
+//ForEach Basics
+names.forEach((name, index) => {
+  console.log(`${name} (${provinces[index]})`);
+});
+
+//toUpperCase
+console.log(provinces.map(province => province.toUpperCase()));
+
+//Name Lengths
+console.log(names.map(name => name.length));
+
+//Sorting
